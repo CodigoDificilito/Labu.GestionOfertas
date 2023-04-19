3a17c807a1fdadb3873e574bf1db38a6cf5f66a5
ivanbrestt <ivanbrestt@gmail.com>
2023-04-19 14:46:26 -0300

    AppDbContext editando

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Infrastructure
+namespace Infrastructure.Persistance
 {
-    internal class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<TipoEstadoPostulacion> TipoEstadoPostulacion { get; set; }
+        public DbSet<Oferta> oferta { get; set; }
+        public DbSet<OfertaCategoria> OfertaCategoria { get; set; }
+        public DbSet<Postulacion> Postulacion { get; set; }
+
     }
 }
